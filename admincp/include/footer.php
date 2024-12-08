6ac5ead502bb71541d182cdc851e6b33f0fc5761
Mai Huyền <165484972+MikeNekkkk@users.noreply.github.com>
2024-12-08 19:40:07 +0700

    Update footer.php

--- admincp/include/footer.php
+++ admincp/include/footer.php
@@ -7,4 +7,3 @@ name="Advertisement" style="admincp"
 src="images/quangcao.swf" 
 type="application/x-shockwave-flash">
 Đồ án Website kinh doanh máy tính xách tay trực tuyến</br>
-Thực hiện: Nguyễn Xuân Quốc - CCHT05A020 - XuanQuocQN@gmail.com - 01657990372
